70edc12c1ae216e98f5acb94b75c1fca6cbc51f0
tatsuroaoki <tatsuro.aoki@gmail.com>
2018-07-12 20:21:54 -0700

    added counter in title and made changes to font when clicks hit 10 and 20

--- js/koala.js
+++ js/koala.js
@@ -1,7 +1,7 @@
 /*----- Javascript Document -----*/
 //1. get the element we want, attach listener
-let clickable = document.querySelector("img.clickable")
 
+let clickable = document.querySelector("img.clickable")
 clickable.addEventListener("click", clickCounter)
 
 let counter = 0
@@ -12,11 +12,18 @@ function clickCounter(){
 	//track total clicks- increment the counter variable
 	//counter = counter + 1
 	counter++
-	
+
 	//output click count to container in DOM
 	console.log("counter", counter)
-	
+
 	//get the container, update its contents
-	let container = document.querySelector("p#clickcount")
-	container.innerHTML = counter
+	let output = document.querySelector("p#clickcount")
+	output.innerHTML = counter
+	document.title = counter
+
+	if (counter === 10 || counter === 20) {
+		output.setAttribute("class", "color")
+	} else {
+		output.removeAttribute("class", "color")
+	}
 }
