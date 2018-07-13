0c120b6b04b3318bdfe6f26230181307ce54048f
archiepv <archiepv@uw.edu>
2018-07-12 19:07:45 -0700

    update

--- js/koala.js
+++ js/koala.js
@@ -1,1 +1,22 @@
+/*----- Javascript Document -----*/
+//1. get the element we want, attach listener
+let clickable = document.querySelector("img.clickable")
 
+clickable.addEventListener("click", clickCounter)
+
+let counter = 0
+console.log("counter: ", counter)
+
+//click handler function
+function clickCounter(){
+	//track total clicks- increment the counter variable
+	//counter = counter + 1
+	counter++
+	
+	//output click count to container in DOM
+	console.log("counter", counter)
+	
+	//get the container, update its contents
+	let container = document.querySelector("p#clickcount")
+	container.innerHTML = counter
+}
